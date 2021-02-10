ba6d5e0334f06c37ac89c1927df20ea417beb9cb
Will <15350912+999gary@users.noreply.github.com>
2021-02-09 23:56:41 -0600

    Bugfix update.

--- ExampleItemMod/ExampleItemMod/ExampleItem.cs
+++ ExampleItemMod/ExampleItemMod/ExampleItem.cs
@@ -16,8 +16,21 @@ namespace ExampleItemMod
     {
         //With a mod api of some kind this shouldn't be required. But for now you need to manually check IDs.
         //Anything over 100000? is a weapon, anything under is a regular item.
-        bladeOPTrash = 200909,
+        bladeBetterDagger = 200909,
 
+    }
+
+
+
+    //Disable bug reporting to the official devs.
+    [HarmonyPatch(typeof(SerializeGO), "ShowBugForm")]
+    class SerializeGOShowBugFormHook
+    {
+        static bool Prefix()
+        {
+            MelonLogger.Log("Please report all bugs to the mod developer. https://github.com/999gary/FTKExampleItemMod");
+            return false;
+        }
     }
 
     [HarmonyPatch(typeof(FTK_itembase), "GetLocalizedName")]
@@ -27,10 +40,10 @@ namespace ExampleItemMod
         {
             //TODO: Automate.
             //If the weapon has our item's ID
-            if (__instance.m_ID == "bladeOPTrash")
+            if (__instance.m_ID == "bladeBetterDagger")
             {
                 //Return our item name.
-                __result = "OP Trash";
+                __result = "Better Dagger";
                 return false;
             }
             return true;
@@ -55,20 +68,40 @@ namespace ExampleItemMod
         }
     }
 
+    [HarmonyPatch(typeof(FTK_weaponStats2DB), "GetIntFromID")]
+    class weaponStats2DBGetIntFromIDHook
+    {
+        static bool Prefix(ref int __result, FTK_weaponStats2DB __instance, string _id)
+        {
+            try
+            {
+                //Attempts to return our enum and calls the original function if it errors.
+                __result = (int)Enum.Parse(typeof(Items), _id, true);
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return true;
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(FTK_itemsDB), "GetEntry")]
     class itemsDBGetEntryHook
     {
         static bool Prefix(FTK_itemsDB __instance, ref FTK_items __result, FTK_itembase.ID _enumID)
         {
             //Never called but here for completeness sake.
-            if ((Items)_enumID == Items.bladeOPTrash)
+            if ((Items)_enumID == Items.bladeBetterDagger)
             {
-                __result = __instance.GetEntryByInt(100109);
+                __result = __instance.GetEntryByInt((int)Items.bladeBetterDagger);
                 return false;
             }
             return true;
         }
     }
+
+
 
     [HarmonyPatch(typeof(FTK_weaponStats2DB), "GetEntry")]
     class weaponStats2DBGetEntryHook
@@ -76,25 +109,70 @@ namespace ExampleItemMod
         static bool Prefix(FTK_weaponStats2DB __instance, ref FTK_weaponStats2 __result, FTK_itembase.ID _enumID)
         {
             //TODO: Automate this process.
-            if ((Items)_enumID == Items.bladeOPTrash)
+            if ((Items)_enumID == Items.bladeBetterDagger)
             {
-                //Copy stats from base item Rusty Blade.
-                __result = __instance.GetEntryByInt(100109);
+                //Copy stats from base item Dagger.
+                __result = __instance.GetEntryByInt((int)FTK_itembase.ID.bladeDagger);
 
-                //Change the ID to our custom ID (REQUIRED).
+                //Make the item ID different.
+                __result.m_ID = "bladeBetterDagger";
 
-                __result.m_ID = "bladeOPTrash";
-                
-                //Change the damage type to magic.
-                __result._dmgtype = FTK_weaponStats2.DamageType.magic;
-                
-                //Give it unreasonable damage.
-                __result._maxdmg = 50000;
+                //Give it different stats.
+                __result._maxdmg = 20;
+
                 return false;
             }
             return true;
         }
     }
+
+    //Hacky fix for bug.
+    [HarmonyPatch(typeof(FTK_weaponStats2DB), "IsContainID")]
+    class weaponStats2DBIsContainIDHook
+    {
+        static bool Prefix(ref bool __result, string _id)
+        {
+            if (_id == "bladeBetterDagger")
+            {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(FTK_itemsDB), "Awake")]
+    class itemsDBAwakeHook
+    {
+        static void Prefix()
+        {
+            if (TableManager.Instance.Get<FTK_itemsDB>().GetEntryByStringID("bladeBetterDagger") == null)
+            {
+                //Add our item to the array.
+                TableManager.Instance.Get<FTK_itemsDB>().AddEntry("bladeBetterDagger");
+
+                //Update the dictionary.
+                TableManager.Instance.Get<FTK_itemsDB>().CheckAndMakeIndex();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(FTK_weaponStats2DB), "Awake")]
+    class weaponStats2DBAwakeHook
+    {
+        static void Prefix()
+        {
+            if (TableManager.Instance.Get<FTK_weaponStats2DB>().GetEntryByStringID("bladeBetterDagger") == null)
+            {
+                //Add our item to the array.
+                TableManager.Instance.Get<FTK_weaponStats2DB>().AddEntry("bladeBetterDagger");
+
+                //Update the dictionary.
+                TableManager.Instance.Get<FTK_weaponStats2DB>().CheckAndMakeIndex();
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(FTK_itembase), "GetEnum")]
     class itembaseGetEnumHook
     {
@@ -102,26 +180,11 @@ namespace ExampleItemMod
         {
             //Not 100% sure if this is required.
             //If item doesn't exist in array.
-            if (TableManager.Instance.Get<FTK_itemsDB>().GetEntryByStringID("bladeOPTrash") == null)
-            {
-                //Add our item to the array.
-                TableManager.Instance.Get<FTK_itemsDB>().AddEntry("bladeOPTrash");
-                
-                //Update the dictionary.
-                TableManager.Instance.Get<FTK_itemsDB>().CheckAndMakeIndex();
-            }
-            if (TableManager.Instance.Get<FTK_weaponStats2DB>().GetEntryByStringID("bladeOPTrash") == null)
-            {
-                //Add our item to the array.
-                TableManager.Instance.Get<FTK_weaponStats2DB>().AddEntry("bladeOPTrash");
 
-                //Update the dictionary.
-                TableManager.Instance.Get<FTK_weaponStats2DB>().CheckAndMakeIndex();
-            }
             //Return our enum if ID matches ours.
-            if (_id == "bladeOPTrash")
+            if (_id == "bladeBetterDagger")
             {
-                __result = (FTK_itembase.ID)Items.bladeOPTrash;
+                __result = (FTK_itembase.ID)Items.bladeBetterDagger;
                 return false;
             }
             return true;
@@ -138,8 +201,8 @@ namespace ExampleItemMod
             if (_enumID == FTK_playerGameStart.ID.hunter)
             {
                 __result = __instance.GetEntryByInt((int)_enumID);
-                __result.m_StartWeapon = (FTK_itembase.ID)(Items.bladeOPTrash);
-                Debug.Log("Hunter hook Activated " + ((FTK_itembase.ID)(Items.bladeOPTrash)).ToString());
+                __result.m_StartWeapon = (FTK_itembase.ID)(Items.bladeBetterDagger);
+                Debug.Log("Hunter hook Activated " + ((FTK_itembase.ID)(Items.bladeBetterDagger)).ToString());
                 return false;
             }
             else
@@ -158,6 +221,12 @@ namespace ExampleItemMod
             var harmony = HarmonyInstance.Create("com.ExampleMod.Item");
             harmony.PatchAll();
             MelonLogger.Log("Methods patched, have fun. :)");
+        }
+
+        public override void OnLevelWasLoaded(int level)
+        {
+            if (level == 1)
+                FTKVersion.Instance.m_VersionNum = FTKVersion.Instance.m_VersionNum + ".modded";
         }
 
     }
